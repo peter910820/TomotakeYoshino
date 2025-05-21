692a0fca59ba6dc08c8bfe41706387fa9fcbc0fd
peter910820 <peter910820@gmail.com>
2025-05-21 21:07:09 +0800

    feat: add shogimove command register

--- bot/register.go
+++ bot/register.go
@@ -59,6 +59,40 @@ func RegisterCommand(s *discordgo.Session) {
 				},
 			},
 		},
+		{
+			Name:        "shogimove",
+			Description: "操作將棋棋子",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "behavior",
+					Description: "選擇進行移動棋子或是打入",
+					Required:    true,
+					Choices: []*discordgo.ApplicationCommandOptionChoice{
+						{
+							Name:  "移動",
+							Value: "move",
+						},
+						{
+							Name:  "打入",
+							Value: "put",
+						},
+					},
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "position",
+					Description: "選擇棋子移動/打入位置",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "support",
+					Description: "選擇輔助提示詞(開發中)",
+					Required:    false,
+				},
+			},
+		},
 	}
 	for _, cmd := range commands {
 		_, err := s.ApplicationCommandCreate(s.State.User.ID, "", cmd)
